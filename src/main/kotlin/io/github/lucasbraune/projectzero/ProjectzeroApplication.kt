9f5b0021af0772a5c466f12dc3602b3d7af47ea1
Lucas Braune <lucasbraune@gmail.com>
2021-04-30 23:22:14 -0300

    Add basic persistence

--- src/main/kotlin/io/github/lucasbraune/projectzero/ProjectzeroApplication.kt
+++ src/main/kotlin/io/github/lucasbraune/projectzero/ProjectzeroApplication.kt
@@ -1,8 +1,13 @@
 package io.github.lucasbraune.projectzero
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.repository.CrudRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
@@ -14,9 +19,19 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MyController {
+class MyController (@field:Autowired private val repository: CrudRepository<Customer, Int>) {
 
 	@GetMapping("/")
 	fun hello(): String = helloPage()
+
+	@GetMapping("/customers")
+	fun getCustomers(): ResponseEntity<List<Customer>> =
+		ResponseEntity.ok(repository.findAll().toList())
+
+	@PostMapping("/customers")
+	fun createCustomer(@RequestBody name: String): ResponseEntity<Customer> {
+		val saved: Customer = repository.save(Customer(0, name))
+		return ResponseEntity.ok(saved)
+	}
 
 }
